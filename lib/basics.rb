318619dc88304caae102ca3e1733a3a33eac670a
Asa Calow <asacalow@alakazam.local>
2010-03-23 09:58:00 +0000

    Basic client now uses digest authentication.

--- lib/basics.rb
+++ lib/basics.rb
@@ -3,15 +3,23 @@ module Bersalis
     attr_accessor :jid, :username, :password
     
     handle Features,                  :ready_to_authenticate,     :filter => '/features/auth:auth', :filter_ns => {'auth' => 'http://jabber.org/features/iq-auth'}
+    handle DigestAuthChallenge,       :auth_challenge
     handle AuthenticationSuccessful,  :authentication_successful
     handle Features,                  :ready_to_bind,             :filter => '/features/bind:bind', :filter_ns => {'bind' => 'urn:ietf:params:xml:ns:xmpp-bind'}
     handle Bind,                      :bound
     handle Session,                   :session_started
 
     def ready_to_authenticate(features)
-      auth = PlainAuth.create
-      auth.set_credentials(:jid => 'jimjam@alakazam.local', :username => 'jimjam', :password => 'password')
+      auth = DigestAuth.create
+      #auth.set_credentials(:jid => 'jimjam@alakazam.local', :username => 'jimjam', :password => 'password')
       write auth
+    end
+    
+    def auth_challenge(challenge)
+      challenge.decode_challenge
+      response = DigestAuthChallengeResponse.create
+      response.set_credentials(:username => 'jimjam', :password => 'password', :nonce => challenge.nonce, :realm => challenge.realm, :domain => 'alakazam.local')
+      write response
     end
 
     def authentication_successful(auth_success)
@@ -25,6 +33,7 @@ module Bersalis
     end
 
     def bound(bind)
+      puts "…bound!"
       write Session.create(:type => 'set')
     end
 
