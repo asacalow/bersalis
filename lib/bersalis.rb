e9cf86b9f9a41a0c411a77014acb7874413b5799
Asa Calow <asacalow@alakazam.local>
2010-03-23 09:57:41 +0000

    Digest Authentication now fully supported.

--- lib/bersalis.rb
+++ lib/bersalis.rb
@@ -1,4 +1,5 @@
 require 'logger'
+require 'digest'
 
 module Bersalis
   VERSION = '0.2'
--- test/stanzas/auth_test.rb
+++ test/stanzas/auth_test.rb
@@ -11,6 +11,11 @@ class AuthTest < Test::Unit::TestCase
     should_create "<auth xmlns=\"urn:ietf:params:xml:ns:xmpp-sasl\" mechanism=\"PLAIN\"/>"
   end
   
+  context 'Bersalis::DigestAuth' do
+    test_stanza "<auth xmlns=\"urn:ietf:params:xml:ns:xmpp-sasl\" mechanism=\"DIGEST-MD5\" />"
+    should_create "<auth xmlns=\"urn:ietf:params:xml:ns:xmpp-sasl\" mechanism=\"DIGEST-MD5\"/>"
+  end
+  
   context 'Bersalis::AuthenticationSuccessful' do
     test_stanza "<success xmlns=\"urn:ietf:params:xml:ns:xmpp-sasl\" />"
   end
