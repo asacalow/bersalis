d9e0a0704b786760c2e001f56efbbb11df052d48
Tekin <tekin@tekin.co.uk>
2010-03-30 14:30:28 +0100

    Updated expected xml in Disco stanza test to match what gets generated - need to update the macro to deal with this situation

--- test/unit/disco_test.rb
+++ test/unit/disco_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class DiscoTest < Test::Unit::TestCase
   context 'Bersalis::DiscoInfo' do
     test_stanza "<iq type=\"get\"><query xmlns=\"http://jabber.org/protocol/disco#info\"/></iq>"
-    should_create("<iq id=\"1\"><query xmlns=\"http://jabber.org/protocol/disco#info\"/></iq>")
+    should_create("<iq id=\"3\"><query xmlns=\"http://jabber.org/protocol/disco#info\"/></iq>")
   end
   
   context 'Bersalis::DiscoItems' do
