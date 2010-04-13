dd7c273eebde43a01c7a5394edf640874465d6a1
Asa Calow <asacalow@alakazam.local>
2010-04-13 09:47:59 +0100

    Added the rubgems dependency (for the examples only) and added a new EchoBot example too.

--- examples/basic_client.rb
+++ examples/basic_client.rb
@@ -1,10 +1,13 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+require 'rubygems'
 require 'bersalis'
+
+include Bersalis
 
 trap(:SIGINT)   { EM.stop }
 trap(:SIGTERM)  { EM.stop }
 
 EM.run do
-  client = Bersalis::BasicClient.new(:jid => 'jimjam@alakazam.local', :password => 'password')
+  client = BasicClient.new(:jid => 'jimjam@alakazam.local', :password => 'password')
   client.connect
 end
--- examples/echo_bot.rb
+++ examples/echo_bot.rb
@@ -1,5 +1,7 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+require 'rubygems'
 require 'bersalis'
+
 include Bersalis
 
 trap(:SIGINT)   { EM.stop }
